d1a0668327efcddb7b1d12c0a0a1914b1233309d
UwUbulachka <meuw25@gmail.com>
2021-09-29 13:38:05 +0500

    Method full_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def full_title(page_title = '')  #full_title(yield(:title))
+    base_title = "Ruby on Rails Tutorial Sample App"
+
+    if page_title.empty? #если заколовок страницы пуст выводи только base title
+        base_title
+    else #иначе выводи заголовк станицы и основной заголовок
+       "#{page_title} | #{base_title}"  
+    end    
+  end
 end
