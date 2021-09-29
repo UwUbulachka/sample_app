2c0270f5607e9b2d343cac609fd83fa7827b7159
UwUbulachka <meuw25@gmail.com>
2021-09-29 12:23:09 +0500

    Backtrace

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -5,3 +5,10 @@
 
 # You can also remove all the silencers if you're trying to debug a problem that might stem from framework code.
 # Rails.backtrace_cleaner.remove_silencers!
+
+# Перезапустите сервер после изменения этого файла
+# Вы можете добавить глушитель для библиотек, которыми пользуетесь, но не хотите видеть
+# в трассировке
+Rails.backtrace_cleaner.add_silencer { |line| line =~ /rvm/ }
+# Также можно удалить все глушители, если проблема происходит из кода фреймворка
+# Rails.backtrace_cleaner.remove_silencers!
