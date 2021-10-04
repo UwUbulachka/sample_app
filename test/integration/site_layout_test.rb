d7e1f020c42e753f56d3ae29f336cc3c5dc85468
UwUbulachka <meuw25@gmail.com>
2021-10-04 13:30:24 +0500

    Link validation tests and Creating controller Users

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+   test "layouts links" do
+     get root_path #выполнить переход по корневому маршруту
+     assert_template 'static_pages/home' #убедиться, что возвращается правильная страница
+     assert_select "a[href=?]", root_path, count: 2 #проверить корректность ссылок на страницы Home, Help, About и Contact
+     assert_select "a[href=?]", help_path
+     assert_select "a[href=?]", about_path
+     assert_select "a[href=?]", contact_path
+   end
+end
