5aafa82ecd840652372a0905842ca912dd75c053
UwUbulachka <meuw25@gmail.com>
2021-10-05 12:15:58 +0500

    Including ApplicationHelper in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,7 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+  include ApplicationHelper #подключем метод full title что бы можно было протестировать
+  
 end
