a093bdb93adc485229c5e5c522788cd17e3c9350
UwUbulachka <meuw25@gmail.com>
2021-10-08 15:50:56 +0500

    User verification

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  before_save{self.email = email.downcase} #до сохранения переведи метод email в нижний ригистр (можно записать так email = email.downcase)
+  validates :name, presence: true, length: {maximum: 50} #проверяет: имя, присутствие: истина
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i #регулярное выражение
+  validates :email, presence: true, length: {maximum: 255},
+                                    format: {with: VALID_EMAIL_REGEX}, #проверка формата 
+                                    uniqueness: {case_sensitive: false} #уникальность чувствительна к регистру ложь
+  has_secure_password #имеет надежный пароль
+  validates :password, length: {minimum: 6} #минимальная длинна пороля
+end
