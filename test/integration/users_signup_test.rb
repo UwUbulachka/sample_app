af0b2002e65bc4a4f6ec2080f86a45c2ed9caf64
UwUbulachka <meuw25@gmail.com>
2021-10-14 13:11:20 +0500

    Successful and unsuccessful registration tests

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do #неверная регистрационная информация
+    get signup_path #получение страницы
+    assert_no_difference 'User.count' do #(утверждать отсутствие разницы)(пользователь не должен создаться при неверной форме)сравнение значения User.count до и после выполнения блока
+      post users_path, params: {user: { name: "", # отправить форму пользователя
+                                        email: "user@invalid",
+                                        password: "foo",
+                                        password_confirmation: "bar" } }
+    end  
+    assert_template 'users/new' # праверка повторного обращения к методу new при ошибочной регистрации
+  end  
+
+  test "valid signup information" do #верная регистрационная информация
+    get signup_path
+    assert_difference 'User.count', 1 do #(утверждать разницу) пользователь должен создоваться (1 не обязтельно)
+      post users_path, params: {user: { name: "Example User", 
+                                    email: "user@example.com",
+                                    password: "password",
+                                    password_confirmation: "password" }}
+    follow_redirect! # переадресацию к отображению шаблона 'users/show' после отправки формы
+    end
+    assert_template 'users/show' # праверка обращения к методу show при удачной регистрации
+  end
+end
