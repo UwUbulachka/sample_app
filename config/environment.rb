6c6e9e1e63fb70f59dbcb6f925eec4b76a44349c
UwUbulachka <meuw25@gmail.com>
2021-10-14 14:00:48 +0500

    Updating

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
-# Load the Rails application.
-require_relative 'application'
+config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-# Initialize the Rails application.
-Rails.application.initialize!
+if ENV["RAILS_LOG_TO_STDOUT"].present?
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
+end
