ee03cd3ea0935740a61e4cdb8b88f4c01d4f3ac8
UwUbulachka <meuw25@gmail.com>
2021-10-14 13:09:50 +0500

    Updating controller Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,23 @@ class UsersController < ApplicationController
   end
     
   def new
+    @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!" #создания кратковременного сообщения
+      redirect_to @user #переадресации на страницу профиля вновь созданного пользователя
+      # Обработать успешное сохранение.
+    else
+    render 'new'  
+    end
+  end  
+
+  private
+
+  def user_params
+   params.require(:user).permit(:name, :email, :password, :password_confirmation) 
+  end  
 end
