5ad44c3baf95a767ace0982f2b36fd4b8bfdc2f1
UwUbulachka <meuw25@gmail.com>
2021-10-17 17:31:42 +0500

    Dropdown button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception #защищать от подделки за исключением
+  include SessionsHelper #подключения модуля Sessions для доступа во всех ктроллерах
 end
