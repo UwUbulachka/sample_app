65097ff4a254bbd69066c5a541febf5c9ab0f411
UwUbulachka <meuw25@gmail.com>
2021-10-18 14:15:47 +0500

    User login test with correct information

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup #создание пользователя
+    @user = users(:michael)
+  end
+    
   test "login with invalid information" do
     get login_path #открыть страницу входа
     assert_template 'sessions/new' #должен отаброжать страницу верно
@@ -9,5 +14,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty? #сообщение об ошибке не должно быть пустым
     get root_path # перейти на другую странцу
     assert flash.empty? #убедитьсяб что там нет сообщения об ошибке
+  end
+
+  test "login with valid information" do
+    get login_path #открыть форму входа
+    post login_path, params: {session: {email: @user.email, password: 'password'}} #отправить существующий логи и пороль
+    assert_redirected_to @user #перенаправить на usera
+    follow_redirect! # открывает страницу
+    assert_template 'users/show' #должен отображать страницу верно
+    assert_select "a[href=?]", login_path, count: 0 # после входа должно быть 0 ссылок на "вход"
+    assert_select "a[href=?]", logout_path #должна появиться ссылка для выхода
+    assert_select "a[href=?]", user_path(@user) # и профиль
   end  
 end
