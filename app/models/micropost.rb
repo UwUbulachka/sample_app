6e1d58ac01c2e215b4da52de766be75eebc8260a
UwUbulachka <meuw25@gmail.com>
2021-11-08 15:16:06 +0500

    User/Micropost  communication

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ApplicationRecord
-  belongs_to :user
+  belongs_to :user #посты пренадлежат пользователю
   validates :user_id, presence: true #проверят индификатор пользователя должен присутствовать
   validates :content, presence: true, length: { maximum: 140 }
 end
