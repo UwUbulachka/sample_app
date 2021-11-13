a1c4aec707bf9141c59b30b2df9027dd30c03aeb
UwUbulachka <meuw25@gmail.com>
2021-11-13 20:25:07 +0500

    update

--- db/migrate/20211107154732_create_microposts.rb
+++ db/migrate/20211107154732_create_microposts.rb
@@ -2,10 +2,11 @@ class CreateMicroposts < ActiveRecord::Migration[5.2]
   def change
     create_table :microposts do |t|
       t.text :content
-      t.references :user, foreign_key: true
+      t.references :user
 
       t.timestamps
     end
+    add_foreign_key :microposts, :users
     add_index :microposts, [:user_id, :created_at] #для обратного порядка
   end
 end
