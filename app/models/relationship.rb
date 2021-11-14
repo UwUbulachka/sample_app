6f65972c9f7d522c5ada7efcf4b642971f054036
UwUbulachka <meuw25@gmail.com>
2021-11-14 21:36:04 +0500

    Checking relationships

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,6 @@
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User" #взаимотношения имеют одного читающего пользователя
   belongs_to :followed, class_name: "User"
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
 end
--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @relationship = Relationship.new(follower_id: 14035331, followed_id: 187601757)
+  end
+  test "should be valid" do
+    assert @relationship.valid?
+  end
+  test "should require a follower_id" do
+    @relationship.follower_id = nil
+    assert_not @relationship.valid?
+  end
+  test "should require a followed_id" do
+    @relationship.followed_id = nil
+    assert_not @relationship.valid?
+  end
 end
