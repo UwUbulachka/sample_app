9e1b2c73ec79a9e7ab2b411aeeded0b33b7ecc73
UwUbulachka <meuw25@gmail.com>
2021-11-15 19:55:25 +0500

    Statistics and form for registration of the follow-up

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,14 @@ Rails.application.routes.draw do
   get 'login'   => 'sessions#new' #Страница представления нового сеанса (вход)
   post 'login'   => 'sessions#create' #Создание нового сеанса (вход)
   delete 'logout'  => 'sessions#destroy' #Удаление сеанса (выход)
+  resources :users do 
+    member do 
+      get :following, :followers # users/1/following, /users/1/followers
+    end  
+  end
   resources :users #rest все
   resources :account_activations, only: [:edit] #rest только для edit 
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end 
